b143c11ce0ee4ba6348043035fc7ab31c0a9bdc2
cinelli.mattia@gmail.com <cinelli.mattia@gmail.com>
2023-08-31 09:34:23 +0000

    first commit

---
+++ MarketAnalysis/Stock Data Analysis.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+from pyspark.sql.functions import max
+
+# COMMAND ----------
+
+df = spark.read.format('delta')\
+    .load('dbfs:/user/hive/warehouse/tesla_data_2020_csv')
+
+display(df)
+
+# COMMAND ----------
+
+df.select(max("Adj_Close"), max("Volume"))\
+    .withColumnRenamed('max(Adj_Close)', "Max_Close")\
+        .withColumnRenamed('max(Volume)', "Max_Volume")\
+            .show(truncate=False)
+
+# COMMAND ----------
+
+
